1b13258e8f903dd6acee7938c172dfc60064297b
elhayounimeriem-sketch <elhayounimeriem@gmail.com>
2026-05-14 17:04:51 +0100

    ajout

---
+++ login.js
@@ -0,0 +1,15 @@
+const login = (event) => {
+    event.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    console.log(username, password);
+
+    if (username === "admin" && password === "admin") {
+        window.location.href = "./home.html";
+    } else {
+        alert("Invalid username or password");
+    }
+};
+
+document.getElementById("login-form").addEventListener("submit", login);
